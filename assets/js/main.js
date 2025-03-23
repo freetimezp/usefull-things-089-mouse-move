eb73dfae24fa97704c19d8663453e52f5ee5538e
freetimezp <kraftukrnet@ukr.net>
2025-03-23 18:43:04 +0200

    02. usefull-things-089-mouse-move. UI. Hover animation.

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+
+const nav = document.getElementById("nav");
+
+window.onmousemove = (e) => {
+    const percent = e.clientX / window.innerWidth;
+
+    nav.animate({
+        transform: `translateX(${percent * nav.offsetWidth * -1}px)`,
+
+    }, {
+        fill: "forwards",
+        duration: 4000
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
