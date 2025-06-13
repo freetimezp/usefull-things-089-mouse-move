14f12ec8f07ed06350fedd4611f146972d2655fa
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-06-13 18:00:58 +0300

    03. usefull-things-089-mouse-move. Bg.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,15 @@
-
 const nav = document.getElementById("nav");
 
 window.onmousemove = (e) => {
     const percent = e.clientX / window.innerWidth;
 
-    nav.animate({
-        transform: `translateX(${percent * nav.offsetWidth * -1}px)`,
-
-    }, {
-        fill: "forwards",
-        duration: 4000
-    });
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    nav.animate(
+        {
+            transform: `translateX(${percent * nav.offsetWidth * -1}px)`,
+        },
+        {
+            fill: "forwards",
+            duration: 256000,
+        }
+    );
+};
